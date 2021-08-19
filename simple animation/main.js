090e04d4d910f28be8ee1813f5bf727840023a94
Shai Dickman <shai.j.dickman@gmail.com>
2021-08-19 12:04:26 -0700

    repurpose with objects

--- simple animation/main.js
+++ simple animation/main.js
@@ -2,11 +2,34 @@
 // wait for the page to finish loading with init as the callback
 window.addEventListener("load", init);
 
+class Ball{
+  constructor(x,y,dx,dy,r,color){
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.r = r;
+    this.color = color;
+  }
+  update = function(){
+    this.x+=this.dx;
+    this.y+=this.dy;
+  }
+  draw = function(){
+    circle(this.x,this.y,this.r,this.color);
+  }
+  checkEdges = function(){
+    if(this.x<=this.r||this.x>=canvas.width-this.r){
+      this.dx*=-1;
+    }
+    if(this.y<=this.r||this.y>=canvas.height-this.r){
+      this.dy*=-1;
+    }
+  }
+}
+
 // global variables
-var colors;
-//var x, y,dx,dy;
-var xArr,yArr,dxArr,dyArr,ballColors;
-var canvas,context,velocity,radius;
+var colors,balls,canvas,context,velocity,radius;
 
 function init(){
     // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
@@ -14,18 +37,12 @@ function init(){
     // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
     context = canvas.getContext("2d");
 
-    radius = 12;
-    velocity = 6;
+    radius = 20;
+    velocity = 7;
     colors = ["orange","blue","red","green","yellow"];
-    /*x = y = 100;    // initial x,y canvas location
-    dx = dy = velocity;*/    // velocity in x and y directions
 
-    //multiple balls with arrays
-    xArr = []; //set position arrays
-    yArr = [];
-    dxArr = [];
-    dyArr = []; //set delta arrays
-    ballColors = [];
+    //multiple balls with array
+    balls = [];
 
     createBalls(5);
     animate();      // kick off the animation
@@ -37,69 +54,41 @@ function createBalls(ballAmount){
     let xPos = radius+Math.random()*(canvas.width-2*radius);
     let yPos = radius+Math.random()*(canvas.height-2*radius);
 
-    let dxVal = (1-2*Math.round(Math.random()))*(Math.floor((velocity/2)*Math.random())+velocity/2);
-    let dyVal = (1-2*Math.round(Math.random()))*(Math.floor((velocity-1)*Math.random())+velocity/2);
+    let dxVal = (1-2*Math.round(Math.random()))*randomVal(velocity);
+    let dyVal = (1-2*Math.round(Math.random()))*randomVal(velocity);
     let ballColor = colors[Math.floor(Math.random()*colors.length)];
 
-    xArr.push(xPos);
-    yArr.push(yPos);
-    dxArr.push(dxVal);
-    dyArr.push(dyVal);
-    ballColors.push(ballColor);
+    let newBall = new Ball(xPos,yPos,dxVal,dyVal,randomVal(radius),ballColor);
+    balls.push(newBall);
 
   }
 
+}
+function randomVal(value){
+  return Math.floor((value-1)*Math.random())+value/2;
 }
 // every animation cycle
 function animate() {
     // erase the HTMLCanvasElement
     context.clearRect(0,0,canvas.width,canvas.height);
-    checkForEdges();
-    update();   // update location
-    draw();     // render
+    update();   // update location   // render
     requestAnimationFrame(animate); // next cycle
 }
 
 // move the circle to a new location
 function update() {
-    //x += dx;    // update x coordinate of location with x velocity
-    //y += dy;    // update y coordinate of location with y velocity
-
-    for(var i = 0;i<xArr.length;i++){
-      xArr[i]+=dxArr[i];
-      yArr[i]+=dyArr[i];
-    }
-}
-
-function checkForEdges(){
-  /*if(x<radius||x>canvas.width-radius){
-    dx*=-1;
-  }
-  if(y<radius||y>canvas.height-radius){
-    dy*=-1;
-  }*/
-  for(var i = 0;i<xArr.length;i++){
-    if(xArr[i]<=radius||xArr[i]>=canvas.width-radius){
-      dxArr[i]*=-1;
-    }
-    if(yArr[i]<=radius||yArr[i]>=canvas.height-radius){
-      dyArr[i]*=-1;
-    }
-  }
-}
-// render a circle
-function draw() {
-    //circle(x,y,"blue");
-    for(var i = 0;i<xArr.length;i++){
-      circle(xArr[i],yArr[i],ballColors[i]);
+    for(var i = 0;i<balls.length;i++){
+      balls[i].checkEdges();
+      balls[i].update();
+      balls[i].draw();
     }
 }
 //makes a circle at a position with a certain colors
-function circle(xPos,yPos,color){
+function circle(xPos,yPos,r,color){
   // create the circle path
   context.beginPath();    // clear old path
   // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
-  context.arc(xPos, yPos, radius, 0, 2 * Math.PI);
+  context.arc(xPos, yPos, r, 0, 2 * Math.PI);
   context.strokeStyle = "black";  // color to fill
   context.fillStyle = color;     // color to stroke
   context.fill();     // render the fill
